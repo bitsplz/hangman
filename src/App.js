4f7ae164de365debab395963f400a48efce6ea87
Muhammad Mujtaba Afzal <mujtabaafzal001@gmail.com>
2020-08-12 19:14:11 +0500

    Components Created
    All the required components Created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import './App.css';
+import Header from './components/header.jsx';
+import Figure from './components/figure.jsx';
+import WrongLetters from './components/wrongletters.jsx';
+import Word from './components/word';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <>
+      <Header/>
+      <div className="game-container">
+        <Figure/>
+        <WrongLetters/>
+        <Word/>
+      </div>
+    </>
   );
 }
 
---
+++ src/components/header.jsx
@@ -0,0 +1,13 @@
+//rafce
+import React from 'react'
+
+const Header = () => {
+    return (
+        <>
+            <h1>Hangman</h1>
+            <p>Find the hidden Word - Enter a letter</p>
+        </>
+    )
+}
+
+export default Header
